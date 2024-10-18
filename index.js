ae5f8ab4ea00c88228fb735a00240cd23d286b4b
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-10-18 16:48:47 +0200

    corrected seconds in 1 hour

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // Given Parameters
 const initVelocity = 2777.78;// convert velocity to m/s to match acceleration unit (m/s)
 const accelerationMeterPerS2 = 3; // acceleration (m/s^2)
-const timeInSeconds = 3600; // seconds (1 hour)
+const timeInSeconds = 3600; // seconds (1 hour) 
 const distanceInKm = 0; // distance (km)
 const initFuelRemaining = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
@@ -18,7 +18,7 @@ const calcNewVel = (accelerationMeterPerS2, initVelocity, timeInSeconds) => {
   return initVelocity + (accelerationMeterPerS2*timeInSeconds)
 }
 
-const newDistance = (initVelocity*timeInSeconds)+(0.5*accelerationMeterPerS2*timeInSeconds*timeInSeconds) //calcultes new distance
+const newDistance = (initVelocity*timeInSeconds)+(0.5*accelerationMeterPerS2*(timeInSeconds**2) )//calcultes new distance
 const endRemaningFuel = initFuelRemaining - (fuelBurnRate*timeInSeconds) //calculates remaining fuel
 const newVelocity = calcNewVel(accelerationMeterPerS2, initVelocity, timeInSeconds) //calculates new velocity based on acceleration
 
