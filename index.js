5e395edafb0c5406b12d54dee55e2663b47b06dd
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-10-18 16:02:25 +0200

    fixed distance calc

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const initFuelRemaining = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const newDistance = distanceInKm + (initVelocity*timeInSeconds) //calcultes new distance
+const newDistance = (initVelocity*timeInSeconds)+(0.5*accelerationMeterPerS2*timeInSeconds^2) //calcultes new distance
 const endRemaningFuel = fuelBurnRate*timeInSeconds //calculates remaining fuel
 const newVelocity = calcNewVel(accelerationMeterPerS2, initVelocity, timeInSeconds) //calculates new velocity based on acceleration
 
